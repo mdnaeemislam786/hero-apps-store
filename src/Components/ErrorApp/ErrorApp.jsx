c29f17938e7690f47676978203e5c0ba5ca5604d
MD NAEEM ISLAM <md.sn.naeem786@gmail.com>
2025-10-10 19:34:22 +0600

    add app not found image and some responsive code of this component

--- src/Components/ErrorApp/ErrorApp.jsx
+++ src/Components/ErrorApp/ErrorApp.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
+import appError from "../../assets/App-Error.png"
 
 const ErrorApp = () => {
     return (
-        <div className='pt-[40vh] bg-red-100 pb-[40vh] flex flex-col justify-center items-center space-y-10'>
-            <h1 className='text-center font-bold text-7xl '>
-                App not found
+        <div className='pt-[20vh] bg-red-100 pb-[20vh] flex flex-col justify-center items-center space-y-10'>
+            <img src={appError} alt="" />
+            <h1 className='text-center font-bold text-2xl md:text-7xl '>
+                OPPS!! APP NOT FOUND
             </h1>
             <Link className='btn bg-linear-to-br text-white from-[#9f62f2] to-[#632ee3]' to="/apps">Go Back</Link>
         </div>
